74fcbfd977632dffb272ec8b7030691e7f9853b1
Sanjarbek012 <sanjarbekxaytboyevv4@gmail.com>
2026-06-06 08:49:10 +0500

    togrlsh

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db.config");
-const authorRouter = require("./router/author.routes");
+
+const authorRouter = require("./router/auth.routes");
 const bookRouter = require("./router/book.routes");
+const quoteRouter = require("./router/quote.routes");
+
 const errorMiddleware = require("./middleware/error.middleware");
 
 const app = express();
@@ -14,14 +17,12 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
 connectDB();
-
-app.use(authorRouter);
-app.use(bookRouter);
-
+app.use("/api/auth", authorRouter);
+app.use("/api/books", bookRouter);
+app.use("/api/quotes", quoteRouter); 
 app.use(errorMiddleware);
 
 app.listen(PORT, () => {
-  console.log("Server is running at: " + PORT);
+    console.log("Server is running at: " + PORT);
 });
